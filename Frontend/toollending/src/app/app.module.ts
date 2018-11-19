d31fc95e0ee88ca70ec6afcf6f4b94f1ac6dc917
zimmem18 <zimmem18@TROMWN4471>
2018-11-19 14:56:44 +0100

    Added Tools, User and Tramscation Components

--- Frontend/toollending/src/app/app.module.ts
+++ Frontend/toollending/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UserComponentComponent } from './user-component/user-component.component';
+import { ToolsComponentComponent } from './tools-component/tools-component.component';
+import { UserComponent } from './user.component';
+import { ToolsComponent } from './tools.component';
+import { TransactionsComponent } from './transactions.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponentComponent,
+    ToolsComponentComponent,
+    UserComponent,
+    ToolsComponent,
+    TransactionsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
