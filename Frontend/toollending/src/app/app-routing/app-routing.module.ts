ac14a78a48bd8f54d04003c259960612b1f0ae07
zimmem18 <zimmem18@TROMWN4471>
2018-11-19 15:46:08 +0100

    Routing implemented

--- Frontend/toollending/src/app/app-routing/app-routing.module.ts
+++ Frontend/toollending/src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from '../components/user.component';
+import { TransactionsComponent } from '../components/transactions.component';
+import { ToolsComponent } from '../components/tools.component';
+
+const routes: Routes = [
+  { path: 'user', component: UserComponent },
+  { path: 'tools',        component: ToolsComponent },
+  { path: 'transaction',        component: TransactionsComponent },
+  { path: '',   redirectTo: '/user', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ],
+    declarations: []
 })
 export class AppRoutingModule { }
--- Frontend/toollending/src/app/app.module.ts
+++ Frontend/toollending/src/app/app.module.ts
@@ -1,28 +1,104 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { UserComponentComponent } from './user-component/user-component.component';
-import { ToolsComponentComponent } from './tools-component/tools-component.component';
-import { UserComponent } from './user.component';
-import { ToolsComponent } from './tools.component';
-import { TransactionsComponent } from './transactions.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './components/user.component';
+import { TransactionsComponent } from './components/transactions.component';
+import { ToolsComponent } from './components/tools.component';
+
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+} from '@angular/material';
+import {CdkTableModule} from '@angular/cdk/table';
+
+@NgModule({
+  exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+  ]
+})
+export class DemoMaterialModule {}
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponentComponent,
-    ToolsComponentComponent,
     UserComponent,
-    ToolsComponent,
     TransactionsComponent,
-    DashboardComponent
+    ToolsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    DemoMaterialModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
